57c20e9949657a62848c427ed61a8b9ac5732b8b
Sarah Na <nsh235482@gmail.com>
2022-03-13 08:30:45 +0900

    17298 오큰수 - 시간 초과

---
+++ nsh/17298/Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    static Stack<Integer> s = new Stack<>();
+    static int[] arr;
+    static int[] answer;
+    static int N;
+
+    static void getNGE() {
+        answer = new int[N];
+        for (int i = N-1; i >= 0; i--) {
+            while (!s.empty() && s.peek() <= arr[i]) {
+                s.pop();
+            }
+
+            if (s.empty()) answer[i] = -1;
+            else answer[i] = s.peek();
+
+            s.push(arr[i]);
+        }
+    }
+
+    static void input() {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            int su = sc.nextInt();
+            arr[i] = su;
+        }
+    }
+    public static void main(String[] args) {
+        input();
+        getNGE();
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(answer[i] + " ");
+        }
+    }
+}
