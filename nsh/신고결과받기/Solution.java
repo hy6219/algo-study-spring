819b1706cbee75a4685ad3f593979c04affc217c
Sarah Na <nsh235482@gmail.com>
2022-03-13 21:14:09 +0900

    설명 주석 추가

--- nsh/신고결과받기/Solution.java
+++ nsh/신고결과받기/Solution.java
@@ -9,13 +9,17 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
+        // 누가 누굴 신고했는지 -> 중복 신고 시 1번으로 간주하므로 set 사용
         Map<String, Set<String>> wh = new HashMap<>();
+        // 신고를 몇번 당했는지
         Map<String, Integer> re = new HashMap<>();
 
+        // 신고 당한 횟수 초기화
         for (int i = 0; i < id_list.length; i++) {
             re.put(id_list[i], 0);
         }
 
+        // 누가 누굴 신고했는지 초기화
         for (int i = 0; i < report.length; i++) {
             String[] r = report[i].split(" ");
             if (wh.get(r[0]) == null) {
@@ -29,6 +33,7 @@ class Solution {
             }
         }
 
+        // 신고 횟수 세기
         for (Set<String> s: wh.values()) {
             Iterator<String> it = s.iterator();
             while(it.hasNext()) {
@@ -37,6 +42,7 @@ class Solution {
             }
         }
 
+        // 신고 횟수가 k 이상인 사람을 갖고 있는 신고자를 찾고, counting
         for (String key : re.keySet()) {
             if (re.get(key) >= k) {
                 for (String ke: wh.keySet()) {
@@ -57,6 +63,6 @@ class Main {
         String[] id_list = {"muzi", "frodo", "apeach", "neo"};
         String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}; 
         solution.solution(id_list, report , 2);
-        
+
     }
 }
